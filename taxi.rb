824d16aa5a3161ae1ee971b59a62871491cbbd41
Oleg Puzanov <puzanov@gmail.com>
2013-02-14 14:14:33 +0600

    Работаем над формой отправки заказа

--- taxi.rb
+++ taxi.rb
@@ -1,10 +1,13 @@
-require "rubygems"
-require "bundler/setup"
-require "sinatra"
-require "net/http"
-require "uri"
-require "json"
-require "memcached"
+require 'rubygems'
+require 'bundler/setup'
+require 'sinatra'
+require 'net/http'
+require 'uri'
+require 'json'
+require 'memcached'
+
+CARS  = '/SmartServerApi/Api/GetFreeDrivers'
+ORDER = '/SmartServerApi/Api/MakeOrderAsIncomingSms'
 
 set :port, 1983
 
@@ -13,20 +16,46 @@ get '/' do
 end
 
 get '/cars' do
-  content_type :json
-
-  $cache = Memcached.new("localhost:11211")
+  $cache = Memcached.new('localhost:11211')
 
   begin
-    @cars = $cache.get "cars"
+    @cars = $cache.get 'cars'
   rescue Memcached::NotFound
-    http = Net::HTTP.new("testnambaapi.zapto.org", 8085)
-    request = Net::HTTP::Post.new("/SmartServerApi/Api/GetFreeDrivers")
-    request.set_form_data({"foo" => "bar"})
-    response = http.request(request)
-    @cars = JSON.parse(response.body)
-    $cache.set "cars", @cars, 60
+    @cars = JSON.parse(make_request_for(CARS, {:foo => 'bar'}).body)
+    $cache.set 'cars', @cars, 60
   end
 
-  @cars["Drivers"].to_json
+  content_type :json
+  @cars['Drivers'].to_json
+end
+
+post '/order' do
+  if params[:address].empty?
+    return {:result => 'error', :message => 'Вы не указали адрес'}.to_json
+  end
+
+  if params[:phone].empty?
+    return {:result => 'error', :message => 'Вы не указали номер вашего телефона'}.to_json
+  end
+
+  if params[:code].empty?
+    return {:result => 'error', :message => 'Вы не указали код номера вашего телефона'}.to_json
+  end
+
+  case make_request_for(ORDER, {:Phone => '+996'+ params[:code] + params[:phone], :Message => params[:address]})
+    when Net::HTTPOK then
+      @result = {:result => 'ok', :message => 'Сейчас наш оператор свяжется с вами'}
+  else
+    @result = {:result => 'error', :message => 'Технические неполадки'}
+  end
+
+  content_type :json
+  @result.to_json
+end
+
+def make_request_for(uri, params)
+  http = Net::HTTP.new('testnambaapi.zapto.org', 8085)
+  request = Net::HTTP::Post.new(uri)
+  request.set_form_data(params)
+  http.request(request)
 end
